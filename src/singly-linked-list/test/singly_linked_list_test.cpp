f2ec89dbb7fedf9ddb7ae1e1a9a8193d819cd0c1
Benjamin Melville <benlyallmelville@gmail.com>
2024-09-27 16:33:49 -0400

    feat: add gtests

--- src/singly-linked-list/test/singly_linked_list_test.cpp
+++ src/singly-linked-list/test/singly_linked_list_test.cpp
@@ -6,9 +6,9 @@
 #include <gtest/gtest.h>
 #include "SinglyLinkedList.h"
 
-TEST(SinglyLinkedListTest, sanityCheck)
+TEST(SinglyLinkedListTest, SanityCheck)
 {
-    ASSERT_EQ(1, 1);
+    ASSERT_TRUE(true);
 }
 
 TEST(SinglyLinkedListTest, AddToFront)
@@ -40,10 +40,158 @@ TEST(SinglyLinkedListTest, AddToFront)
         current = current->next;
         ++current_index;
     }
+    ASSERT_EQ(current->data, expectedValues.back());
 
 }
 
+TEST(SinglyLinkedListTest, AddToBack)
+{
+    std::vector<int> expectedValues;
+    expectedValues.push_back(6);
+    expectedValues.push_back(68);
+    expectedValues.push_back(89);
+    expectedValues.push_back(60);
+    expectedValues.push_back(78);
+    expectedValues.push_back(45);
 
+    SinglyLinkedList myList;
+    myList.addToBack(6);
+    myList.addToBack(68);
+    myList.addToBack(89);
+    myList.addToBack(60);
+    myList.addToBack(78);
+    myList.addToBack(45);
+
+    ASSERT_EQ(myList.getHead()->data, 6);
+
+    Node* current = myList.getHead();
+    int current_index = 0;
+
+    while(current->next != nullptr)
+    {
+        ASSERT_EQ(current->data, expectedValues[current_index]);
+        current = current->next;
+        ++current_index;
+    }
+
+    ASSERT_EQ(current->data, expectedValues.back());
+
+}
+
+TEST(SinglyLinkedListTest, RemoveFromFront)
+{
+
+    std::vector<int> expectedValues;
+    expectedValues.push_back(89);
+    expectedValues.push_back(68);
+    expectedValues.push_back(6);
+
+    SinglyLinkedList myList;
+    myList.addToFront(6);
+    myList.addToFront(68);
+    myList.addToFront(89);
+    myList.addToFront(60);
+    myList.addToFront(78);
+    myList.addToFront(45);
+
+    myList.removeFromFront();
+    myList.removeFromFront();
+    myList.removeFromFront();
+
+    Node* current = myList.getHead();
+    int current_index = 0;
+
+    while(current->next != nullptr)
+    {
+        ASSERT_EQ(current->data, expectedValues[current_index]);
+        current = current->next;
+        ++current_index;
+    }
+
+    ASSERT_EQ(current->data, expectedValues.back());
+}
+
+TEST(SinglyLinkedListTest, RemoveFromBack)
+{
+    std::vector<int> expectedValues;
+    expectedValues.push_back(89);
+    expectedValues.push_back(68);
+    expectedValues.push_back(6);
+
+    SinglyLinkedList myList;
+    myList.addToBack(89);
+    myList.addToBack(68);
+    myList.addToBack(6);
+    myList.addToBack(60);
+    myList.addToBack(78);
+    myList.addToBack(45);
+
+    myList.removeFromBack();
+    myList.removeFromBack();
+    myList.removeFromBack();
+
+    Node* current = myList.getHead();
+    int current_index = 0;
+
+    while(current->next != nullptr)
+    {
+        ASSERT_EQ(current->data, expectedValues[current_index]);
+        current = current->next;
+        ++current_index;
+    }
+
+    ASSERT_EQ(current->data, expectedValues.back());
+}
+
+TEST(SinglyLinkedListTest, RemoveFromFrontEmptyList)
+{
+    SinglyLinkedList myList;
+    myList.removeFromFront();
+    EXPECT_EQ(myList.getHead(), nullptr);
+}
+
+TEST(SinglyLinkedListTest, RemoveFromBackEmptyList)
+{
+    SinglyLinkedList myList;
+    myList.removeFromBack();
+    EXPECT_EQ(myList.getHead(), nullptr);
+}
+
+TEST(SinglyLinkedListTest, RemoveFromFrontSingleNodeList)
+{
+    SinglyLinkedList myList;
+    myList.addToBack(5);
+    myList.removeFromFront();
+    EXPECT_EQ(myList.getHead(), nullptr);
+}
+
+TEST(SinglyLinkedListTest, RemoveFromBackSingleNodeList)
+{
+    SinglyLinkedList myList;
+    myList.addToBack(5);
+    myList.removeFromBack();
+    EXPECT_EQ(myList.getHead(), nullptr);
+}
+
+TEST(SinglyLinkedListTest, RemoveFromBackTwoNodeList)
+{
+    SinglyLinkedList myList;
+    myList.addToBack(5);
+    myList.addToBack(6);
+    myList.removeFromBack();
+    EXPECT_EQ(myList.getHead()->next, nullptr);
+}
+
+
+TEST(SinglyLinkedListTest, RemoveFromBackThreeNodeList)
+{
+    SinglyLinkedList myList;
+    myList.addToBack(5);
+    myList.addToBack(6);
+    myList.addToBack(7);
+    myList.removeFromBack();
+    EXPECT_EQ(myList.getHead()->next->next, nullptr);
+}
 
 int main()
 {
