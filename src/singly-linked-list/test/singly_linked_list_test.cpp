fe3011386bb74b0535036c6e8ef06470b3f45f91
Benjamin Melville <benlyallmelville@gmail.com>
2024-09-27 13:53:41 -0400

    feat: setup initial test environment

---
+++ src/singly-linked-list/test/singly_linked_list_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Benjamin Melville on 9/27/24.
+//
+#include <gtest/gtest.h>
+
+TEST(SinglyLinkedListTest, SanityCheck)
+{
+    ASSERT_EQ(1, 1);
+}
+
+
+int main()
+{
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
