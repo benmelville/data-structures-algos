1ef64d204be48b64defc97f57141624953431545
Benjamin Melville <benlyallmelville@gmail.com>
2024-10-03 21:03:56 -0400

    feat: add testing structure for stack.

--- src/stack/Stack.h
+++ src/stack/Stack.h
@@ -6,6 +6,7 @@
 #define DATA_STRUCTURES_ALGOS_STACK_H
 
 #include "SinglyLinkedList.h"
+#include <iostream>
 
 template<typename T>
 class Stack
--- src/stack/test/stack_test.cpp
+++ src/stack/test/stack_test.cpp
@@ -2,4 +2,35 @@
 // Created by Benjamin Melville on 10/2/24.
 //
 #include "Stack.h"
+#include <gtest/gtest.h>
 
+
+TEST(StackTest, SanityCheck)
+{
+    ASSERT_TRUE(true);
+}
+
+TEST(StackTest, PushTest)
+{
+    //TODO: add test here.
+}
+
+TEST(StackTest, PopTest)
+{
+    //TODO: add test here.
+}
+
+TEST(StackTest, PeekTest)
+{
+    //TODO: add test here.
+}
+
+TEST(StackTest, IsEmptyTest)
+{
+    //TODO: add test here.
+}
+
+TEST(StackTest, ClearTest)
+{
+    //TODO: add test here.
+}
